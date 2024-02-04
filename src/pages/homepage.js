7355cccece70d6444b08ce8fed0f75396323e459
smallzhazha <adneskhorkaisiang@gmail.com>
2024-02-05 06:43:18 +0800

    Homepage and budget initial setup

---
+++ src/pages/homepage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import '../styles/homepage.css'
+
+export default function Homepage(){
+    return(
+        <div>
+            <p>Hello world</p>
+        </div>
+    )
+}
